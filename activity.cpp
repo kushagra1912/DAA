858222cba5a7af0f48a04d848b7d002084704d3e
Kushagra Agrawal <agrawalkushagra647@gmail.com>
2022-05-10 12:09:28 +0530

    activitiy selection

--- activity.cpp
+++ activity.cpp
@@ -4,12 +4,9 @@ using namespace std;
 void printMaxActivities(int s[], int f[], int n)
 {
 	int i, j;
-
 	cout <<"Following activities are selected "<< endl;
-
 	i = 0;
 	cout <<" "<< i;
-
 	for (j = 1; j < n; j++)
 	{
 	if (s[j] >= f[i])
