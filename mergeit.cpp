d7eac74d5389ed27b6b91e4d6a7f77d20758494f
Kushagra Agrawal <agrawalkushagra647@gmail.com>
2022-05-10 13:18:46 +0530

    merge sort

--- mergeit.cpp
+++ mergeit.cpp
@@ -1,23 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 void merge(int arr[], int l, int m, int r);
+
 int min(int x, int y) { return (x<y)? x :y; }
-void mergeSort(int arr[], int n)
-{
-int curr_size;
-int left_start;
-for (curr_size=1; curr_size<=n-1; curr_size = 2*curr_size)
-{
-	for (left_start=0; left_start<n-1; left_start += 2*curr_size)
-	{
-		int mid = min(left_start + curr_size - 1, n-1);
-		int right_end = min(left_start + 2*curr_size - 1, n-1);
-		merge(arr, left_start, mid, right_end);
+
+void mergeSort(int arr[], int n){
+	int curr_size;
+	int left_start;
+	for (curr_size=1; curr_size<=n-1; curr_size = 2*curr_size){
+		for (left_start=0; left_start<n-1; left_start += 2*curr_size)	{
+			int mid = min(left_start + curr_size - 1, n-1);
+			int right_end = min(left_start + 2*curr_size - 1, n-1);
+			merge(arr, left_start, mid, right_end);
+		}
 	}
 }
-}
-void merge(int arr[], int l, int m, int r)
-{
+void merge(int arr[], int l, int m, int r){
 	int i, j, k;
 	int n1 = m - l + 1;
 	int n2 = r - m;
@@ -29,42 +28,35 @@ void merge(int arr[], int l, int m, int r)
 	i = 0;
 	j = 0;
 	k = l;
-	while (i < n1 && j < n2)
-	{
-		if (L[i] <= R[j])
-		{
+	while (i < n1 && j < n2)	{
+		if (L[i] <= R[j])		{
 			arr[k] = L[i];
 			i++;
 		}
-		else
-		{
+		else		{
 			arr[k] = R[j];
 			j++;
 		}
 		k++;
 	}
-	while (i < n1)
-	{
+	while (i < n1)	{
 		arr[k] = L[i];
 		i++;
 		k++;
 	}
-	while (j < n2)
-	{
+	while (j < n2)	{
 		arr[k] = R[j];
 		j++;
 		k++;
 	}
 }
-void printArray(int A[], int size)
-{
+void printArray(int A[], int size){
 	int i;
 	for (i=0; i < size; i++)
 		cout <<" "<< A[i];
 	cout <<"\n";
 }
-int main()
-{
+int main(){
 	int arr[] = {12, 11, 13, 5, 6, 7};
 	int n = sizeof(arr)/sizeof(arr[0]);
 
